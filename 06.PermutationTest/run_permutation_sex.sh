1eddd0bf81042a68ce2e7eb225df4b519d65ec77
Lin-Yuying <61513895+Lin-Yuying@users.noreply.github.com>
2022-02-23 11:32:17 -0800

    Update run_permutation_sex.sh

--- 06.PermutationTest/run_permutation_sex.sh
+++ 06.PermutationTest/run_permutation_sex.sh
@@ -4,6 +4,7 @@ while [ $sim -lt `1000 + 1` ]
 do
 	echo "############ Simulation ${sim} ###############" >> pop.perm.log
 	# please check sample_120.txt for sample groups information
+	# here, we grouped samples according to their sex
 	python permutation.py sample_120.txt male_${sim}.txt female_${sim}.txt 
 	vcftools --vcf sex_GQ30_0.9.recode.vcf --weir-fst-pop male_${sim}  --weir-fst-pop female_${sim}.txt --out pop.snp.${sim}
 	sim=`expr ${sim} + 1`
