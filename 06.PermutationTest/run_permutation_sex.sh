814c03936d604e585282a58265d93b65e2dc7b3d
Lin-Yuying <61513895+Lin-Yuying@users.noreply.github.com>
2022-03-04 15:01:35 -0800

    Update run_permutation_sex.sh

--- 06.PermutationTest/run_permutation_sex.sh
+++ 06.PermutationTest/run_permutation_sex.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-sim = 1
+sim=1
+vcf=2
 while [ $sim -lt `1000 + 1` ]
 do
 	echo "############ Simulation ${sim} ###############" >> pop.perm.log
 	# please check sample_120.txt for sample groups information
 	# here, we grouped samples according to their sex
 	python permutation.py sample_120.txt male_${sim}.txt female_${sim}.txt 
-	vcftools --vcf sex_GQ30_0.9.recode.vcf --weir-fst-pop male_${sim}  --weir-fst-pop female_${sim}.txt --out pop.snp.${sim}
+	vcftools --vcf ${vcf} --weir-fst-pop male_${sim}  --weir-fst-pop female_${sim}.txt --out pop.snp.${sim}
 	sim=`expr ${sim} + 1`
 done
 # header
